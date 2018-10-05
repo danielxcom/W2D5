e25c64d5c50fa9c23e0194d4297627db7772a05c
danielxcom <danixu12@gmail.com>
2018-10-05 17:50:08 -0400

    hashset

--- skeleton/lib/p03_hash_set.rb
+++ skeleton/lib/p03_hash_set.rb
@@ -7,17 +7,33 @@ class HashSet
   end
 
   def insert(key)
+    resize! if count == num_buckets
+    unless self.include?(key)
+      self[key] << key
+      @count += 1
+    end
+    nil
   end
 
   def include?(key)
+    keys = self.keys?
+    keys.each do |kCmpr|
+      return true if key == kCmpr
+    end
+    return false
   end
 
   def remove(key)
+    if self.include?(key)
+      self[key].delete(key)
+      @count -= 1
+    end
   end
 
   private
 
   def [](num)
+    @store[num.hash % num_buckets]
     # optional but useful; return the bucket corresponding to `num`
   end
 
@@ -26,5 +42,12 @@ class HashSet
   end
 
   def resize!
+    newArr = Array.new(count * 2) {Array.new}
+    @store.each do |bucket|
+      bucket.each do |el|
+        newArr[el % newArr.count] << el
+      end
+    end
+    @store = newArr
   end
 end
