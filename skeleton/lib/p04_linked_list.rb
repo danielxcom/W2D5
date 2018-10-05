0a92340b7b9214b30966c328ebdb5de610ff2614
danielxcom <danixu12@gmail.com>
2018-10-05 17:55:46 -0400

    almost finished linked_list

--- skeleton/lib/p04_linked_list.rb
+++ skeleton/lib/p04_linked_list.rb
@@ -28,27 +28,48 @@ class LinkedList
   end
 
   def first
+    @list.first
   end
 
   def last
+    @list.last
   end
 
   def empty?
+    list.empty?
   end
 
   def get(key)
+    self.each do |child_node|
+      return child_node.val if child_node.key == key
+    end
+    nil
   end
 
   def include?(key)
+    self[key].include?(key)
   end
 
   def append(key, val)
+    @list << Node.new(key, val)
   end
 
   def update(key, val)
+    self.each do |child_node|
+      next if child_node.key == key
+        child_node.val
+      end
+    end
   end
 
   def remove(key)
+    self.each do |child_node|
+      if child_node.key == key
+        @store.delete(child_node)
+        break
+      end
+    end
+    nil
   end
 
   def each
